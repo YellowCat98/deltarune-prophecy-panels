59eedda2516202e3214e6290639e1e7975c2a468
YellowCat98 <elcucuyisgay@gmail.com>
2026-07-17 17:39:26 +0100

    alright we can now: Load Dynamically........

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,24 @@ class $modify(MyLoadingLayer, MenuLayer) {
 	}
 
 	void onMoreGames(CCObject* sender) {
-		auto scene = CCScene::create();
-		auto prophecy = DeltaruneProphecy::ProphecyPanel::create(CCSprite::create("test_spr.png"_spr), "THE FLOWER MAN,\nTRAPPED IN ASYLUM.");
-		prophecy->setScale(4.0f);
-		prophecy->setPosition(CCDirector::get()->getWinSize()/2);
+		auto sprite = LazySprite::create({0.0f, 0.0f}); // lazysprite handles it
+		sprite->loadFromFile(Mod::get()->getSettingValue<std::filesystem::path>("sprite"));
 
-		scene->addChild(prophecy);
+		sprite->setLoadCallback([sprite](Result<> res) {
+			if (res.isErr()) return;
+			sprite->removeFromParentAndCleanup(true);
 
-		CCDirector::get()->replaceScene(scene);
+			auto scene = CCScene::create();
+			auto prophecy = DeltaruneProphecy::ProphecyPanel::create(sprite, "THE FLOWER MAN,\nTRAPPED IN ASYLUM.");
+			prophecy->setScale(4.0f);
+			prophecy->setPosition(CCDirector::get()->getWinSize()/2);
+
+			scene->addChild(prophecy);
+
+			CCDirector::get()->replaceScene(scene);
+		});
+
+		this->addChild(sprite); // only go to the prophecy panel once its loaded and the only way to do that is by adding this somewhere (gets removes on load tho)
+
 	}
 };
